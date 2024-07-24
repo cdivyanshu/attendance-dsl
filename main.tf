fbfdf07669d732e7d49352505401388ec5917a31
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-25 01:08:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-# Provider Configuration
 provider "aws" {
   region = "us-west-2"
 }
 
-# VPC
+# Define the VPC
 resource "aws_vpc" "ot_microservices_dev" {
   cidr_block = "10.0.0.0/16"
 
@@ -36,27 +35,12 @@ resource "aws_route_table" "route_table" {
 }
 
 # Associate Route Table with Subnets
-resource "aws_route_table_association" "database_subnet_association" {
-  subnet_id      = aws_subnet.database_subnet.id
-  route_table_id = aws_route_table.route_table.id
-}
-
 resource "aws_route_table_association" "application_subnet_association" {
   subnet_id      = aws_subnet.application_subnet.id
   route_table_id = aws_route_table.route_table.id
 }
 
 # Subnets
-resource "aws_subnet" "database_subnet" {
-  vpc_id            = aws_vpc.ot_microservices_dev.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "database-subnet"
-  }
-}
-
 resource "aws_subnet" "application_subnet" {
   vpc_id            = aws_vpc.ot_microservices_dev.id
   cidr_block        = "10.0.2.0/24"
@@ -91,37 +75,13 @@ resource "aws_security_group" "alb_security_group" {
   }
 }
 
-# Bastion Security Group
-resource "aws_security_group" "bastion_security_group" {
-  vpc_id = aws_vpc.ot_microservices_dev.id
-  name   = "bastion-security-group"
-
-  tags = {
-    Name = "bastion-security-group"
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 # ALB Load Balancer
 resource "aws_lb" "front_end" {
   name               = "frontend-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_security_group.id]
-  subnets            = [aws_subnet.database_subnet.id, aws_subnet.application_subnet.id]
+  subnets            = [aws_subnet.application_subnet.id]
 
   enable_deletion_protection = false
 
@@ -146,14 +106,13 @@ resource "aws_lb_listener" "front_end" {
   }
 }
 
-# ATTENDANCE
-
-resource "aws_security_group" "attendance_security_group" {
+# Define the security group for the employee
+resource "aws_security_group" "employee_security_group" {
   vpc_id = aws_vpc.ot_microservices_dev.id
-  name   = "attendance-security-group"
+  name   = "employee-security-group"
 
   tags = {
-    Name = "attendance-security-group"
+    Name = "employee-security-group"
   }
 
   ingress {
@@ -178,57 +137,63 @@ resource "aws_security_group" "attendance_security_group" {
   }
 }
 
-# Instance
-
-resource "aws_instance" "attendance_instance" {
-  ami                    = "ami-0075013580f6322a1"
-  subnet_id              = aws_subnet.database_subnet.id
-  vpc_security_group_ids = [aws_security_group.attendance_security_group.id]
+# Define the EC2 instance for the employee
+resource "aws_instance" "employee_instance" {
+  ami                    = "ami-0cc489ff5815b317c"
+  subnet_id              = aws_subnet.application_subnet.id
+  vpc_security_group_ids = [aws_security_group.employee_security_group.id]
   instance_type          = "t2.micro"
   key_name               = "backend"
 
   tags = {
-    Name = "Attendance"
+    Name = "employee"
   }
 }
 
-# Target Group and Attachment
-
-resource "aws_lb_target_group" "attendance_target_group" {
-  name     = "attendance-tg"
-  port     = 80
-  protocol = "HTTP"
+# Define the target group for the load balancer
+resource "aws_lb_target_group" "employee_target_group" {
+  name        = "employee-tg"
+  port        = 80
+  protocol    = "HTTP"
   target_type = "instance"
-  vpc_id   = aws_vpc.ot_microservices_dev.id
+  vpc_id      = aws_vpc.ot_microservices_dev.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold    = 2
+    unhealthy_threshold  = 2
+  }
 }
 
-resource "aws_lb_target_group_attachment" "attendance_target_group_attachment" {
-  target_group_arn = aws_lb_target_group.attendance_target_group.arn
-  target_id        = aws_instance.attendance_instance.id
+# Define the target group attachment
+resource "aws_lb_target_group_attachment" "employee_target_group_attachment" {
+  target_group_arn = aws_lb_target_group.employee_target_group.arn
+  target_id        = aws_instance.employee_instance.id
   port             = 8080
 }
 
-# Listener Rule
-
-resource "aws_lb_listener_rule" "attendance_rule" {
+# Define the listener rule for the load balancer
+resource "aws_lb_listener_rule" "employee_rule" {
   listener_arn = aws_lb_listener.front_end.arn
-  priority     = 4
+  priority     = 5
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.attendance_target_group.arn
+    target_group_arn = aws_lb_target_group.employee_target_group.arn
   }
 
   condition {
     path_pattern {
-      values = ["/api/v1/attendance/*"]
+      values = ["/api/v1/employee/*"]
     }
   }
 }
 
-# Launch Template for Attendance
-resource "aws_launch_template" "attendance_launch_template" {
-  name = "attendance-template-unique"  # Updated Launch Template name
+# Define the launch template for the employee instances
+resource "aws_launch_template" "employee_launch_template" {
+  name = "employee-template"
 
   block_device_mappings {
     device_name = "/dev/sdf"
@@ -242,44 +207,44 @@ resource "aws_launch_template" "attendance_launch_template" {
   network_interfaces {
     subnet_id                   = aws_subnet.application_subnet.id
     associate_public_ip_address = false
-    security_groups             = [aws_security_group.attendance_security_group.id]
+    security_groups             = [aws_security_group.employee_security_group.id]
   }
 
   key_name      = "backend"
-  image_id      = "ami-0075013580f6322a1"
+  image_id      = "ami-0cc489ff5815b317c"
   instance_type = "t2.micro"
 
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name = "AttendanceASG"
+      Name = "employeeASG"
     }
   }
 }
 
-# Auto Scaling for Attendance
-
-resource "aws_autoscaling_group" "attendance_autoscaling" {
-  name                      = "attendance-autoscale"
+# Define the auto-scaling group for the employee
+resource "aws_autoscaling_group" "employee_autoscaling" {
+  name                      = "employee-autoscale"
   max_size                  = 2
-  min_size                  = 0
-  desired_capacity          = 0
+  min_size                  = 1
+  desired_capacity          = 1
   health_check_grace_period = 300
   launch_template {
-    id      = aws_launch_template.attendance_launch_template.id
+    id      = aws_launch_template.employee_launch_template.id
     version = "$Default"
   }
   vpc_zone_identifier = [aws_subnet.application_subnet.id]
-  target_group_arns = [aws_lb_target_group.attendance_target_group.arn]
+  target_group_arns = [aws_lb_target_group.employee_target_group.arn]
 }
 
-resource "aws_autoscaling_policy" "attendance" {
-  name                        = "attendance-autoscaling-policy"
-  policy_type                 = "TargetTrackingScaling"
-  adjustment_type             = "ChangeInCapacity"
-  estimated_instance_warmup   = 300
-  autoscaling_group_name      = aws_autoscaling_group.attendance_autoscaling.name
+# Define the auto-scaling policy for the employee
+resource "aws_autoscaling_policy" "employee_scaling_policy" {
+  name                   = "employee-autoscaling-policy"
+  policy_type            = "TargetTrackingScaling"
+  adjustment_type        = "ChangeInCapacity"
+  estimated_instance_warmup = 300
+  autoscaling_group_name = aws_autoscaling_group.employee_autoscaling.name
 
   target_tracking_configuration {
     predefined_metric_specification {
